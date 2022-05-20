6018d4611f928d59993129b2d050ec634a6f0f70
ogabeksanoyev06 <ogabeksanoyev06@gmail.com>
2022-05-20 12:47:45 +0500

    ...

--- src/components/catalog/v-catalog.vue
+++ src/components/catalog/v-catalog.vue
@@ -1,91 +1,93 @@
 <template>
 	<div class="v-catalog">
-		<p class="title d-block mb-3">Shop The Latest</p>
 		<div class="row">
 			<div class="col-md-3 mb-4 mb-md-0">
-				<div class="filter_by">
-					<div class="input_by">
-						<div>
-							<input type="text" v-model="category" placeholder="Search..." />
+				<div class="stiky_x">
+					<p class="title d-block mb-3">Shop The Latest</p>
+					<div class="filter_by">
+						<div class="input_by">
+							<div>
+								<input type="text" v-model="category" placeholder="Search..." />
+							</div>
+							<img src="@/assets/cart-logo/search.png" alt="" />
 						</div>
-						<img src="@/assets/cart-logo/search.png" alt="" />
 					</div>
-				</div>
-				<div class="category_filter mt-3">
-					<div class="selected_item" @click="isVisibleBy = !isVisibleBy">
-						<span>{{
-							shopByItem.slice(0, 1).toUpperCase() + shopByItem.slice(1)
-						}}</span>
-						<svg
-							class="rotate_icon"
-							:class="isVisibleBy ? 'rotate' : ''"
-							width="14"
-							height="8"
-							viewBox="0 0 14 8"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M12.6551 0.103514C12.7208 0.0372643 12.8103 -5.20037e-08 12.9036 -4.7925e-08C12.9969 -4.38464e-08 13.0864 0.0372643 13.1521 0.103514L13.894 0.845401C13.9616 0.908905 14 0.997568 14 1.09036C14 1.18316 13.9616 1.27182 13.894 1.33533L7.46193 7.76735C7.36358 7.86582 7.23016 7.9212 7.09099 7.92133L6.90901 7.92133C6.76984 7.9212 6.63642 7.86582 6.53807 7.76735L0.106047 1.33533C0.0383834 1.27182 9.01957e-07 1.18316 9.06013e-07 1.09036C9.10069e-07 0.997567 0.0383834 0.908904 0.106047 0.8454L0.847934 0.103513C0.913642 0.0372638 1.00309 -5.68113e-07 1.0964 -5.64034e-07C1.18971 -5.59956e-07 1.27915 0.0372638 1.34486 0.103513L7 5.75865L12.6551 0.103514Z"
-								fill="black"
-							/>
-						</svg>
-					</div>
-					<div :class="isVisibleBy ? 'visible' : ''" class="options">
-						<ul>
-							<li
-								v-for="(shopBy, index) in shopBys"
-								:key="index"
-								@click="selectItemBy(shopBy.name)"
+					<div class="category_filter mt-3">
+						<div class="selected_item" @click="isVisibleBy = !isVisibleBy">
+							<span>{{
+								shopByItem.slice(0, 1).toUpperCase() + shopByItem.slice(1)
+							}}</span>
+							<svg
+								class="rotate_icon"
+								:class="isVisibleBy ? 'rotate' : ''"
+								width="14"
+								height="8"
+								viewBox="0 0 14 8"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
 							>
-								{{
-									shopBy.name.slice(0, 1).toUpperCase() + shopBy.name.slice(1)
-								}}
-							</li>
-						</ul>
+								<path
+									d="M12.6551 0.103514C12.7208 0.0372643 12.8103 -5.20037e-08 12.9036 -4.7925e-08C12.9969 -4.38464e-08 13.0864 0.0372643 13.1521 0.103514L13.894 0.845401C13.9616 0.908905 14 0.997568 14 1.09036C14 1.18316 13.9616 1.27182 13.894 1.33533L7.46193 7.76735C7.36358 7.86582 7.23016 7.9212 7.09099 7.92133L6.90901 7.92133C6.76984 7.9212 6.63642 7.86582 6.53807 7.76735L0.106047 1.33533C0.0383834 1.27182 9.01957e-07 1.18316 9.06013e-07 1.09036C9.10069e-07 0.997567 0.0383834 0.908904 0.106047 0.8454L0.847934 0.103513C0.913642 0.0372638 1.00309 -5.68113e-07 1.0964 -5.64034e-07C1.18971 -5.59956e-07 1.27915 0.0372638 1.34486 0.103513L7 5.75865L12.6551 0.103514Z"
+									fill="black"
+								/>
+							</svg>
+						</div>
+						<div :class="isVisibleBy ? 'visible' : ''" class="options">
+							<ul>
+								<li
+									v-for="(shopBy, index) in shopBys"
+									:key="index"
+									@click="selectItemBy(shopBy.name)"
+								>
+									{{
+										shopBy.name.slice(0, 1).toUpperCase() + shopBy.name.slice(1)
+									}}
+								</li>
+							</ul>
+						</div>
 					</div>
-				</div>
-				<div class="category_filter mt-3">
-					<div class="selected_item" @click="isVisible = !isVisible">
-						<span>{{
-							selectedItem.slice(0, 1).toUpperCase() + selectedItem.slice(1)
-						}}</span>
-						<svg
-							class="rotate_icon"
-							:class="isVisible ? 'rotate' : ''"
-							width="14"
-							height="8"
-							viewBox="0 0 14 8"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M12.6551 0.103514C12.7208 0.0372643 12.8103 -5.20037e-08 12.9036 -4.7925e-08C12.9969 -4.38464e-08 13.0864 0.0372643 13.1521 0.103514L13.894 0.845401C13.9616 0.908905 14 0.997568 14 1.09036C14 1.18316 13.9616 1.27182 13.894 1.33533L7.46193 7.76735C7.36358 7.86582 7.23016 7.9212 7.09099 7.92133L6.90901 7.92133C6.76984 7.9212 6.63642 7.86582 6.53807 7.76735L0.106047 1.33533C0.0383834 1.27182 9.01957e-07 1.18316 9.06013e-07 1.09036C9.10069e-07 0.997567 0.0383834 0.908904 0.106047 0.8454L0.847934 0.103513C0.913642 0.0372638 1.00309 -5.68113e-07 1.0964 -5.64034e-07C1.18971 -5.59956e-07 1.27915 0.0372638 1.34486 0.103513L7 5.75865L12.6551 0.103514Z"
-								fill="black"
-							/>
-						</svg>
-					</div>
-					<div :class="isVisible ? 'visible' : ''" class="options">
-						<ul>
-							<li
-								v-for="(selectedCategory, index) in selectedCategorys"
-								:key="index"
-								@click="
-									selectItem(selectedCategory.name),
-										sortByCategory(selectedCategory.name)
-								"
+					<div class="category_filter mt-3">
+						<div class="selected_item" @click="isVisible = !isVisible">
+							<span>{{
+								selectedItem.slice(0, 1).toUpperCase() + selectedItem.slice(1)
+							}}</span>
+							<svg
+								class="rotate_icon"
+								:class="isVisible ? 'rotate' : ''"
+								width="14"
+								height="8"
+								viewBox="0 0 14 8"
+								fill="none"
+								xmlns="http://www.w3.org/2000/svg"
 							>
-								{{
-									selectedCategory.name.slice(0, 1).toUpperCase() +
-									selectedCategory.name.slice(1)
-								}}
-							</li>
-						</ul>
+								<path
+									d="M12.6551 0.103514C12.7208 0.0372643 12.8103 -5.20037e-08 12.9036 -4.7925e-08C12.9969 -4.38464e-08 13.0864 0.0372643 13.1521 0.103514L13.894 0.845401C13.9616 0.908905 14 0.997568 14 1.09036C14 1.18316 13.9616 1.27182 13.894 1.33533L7.46193 7.76735C7.36358 7.86582 7.23016 7.9212 7.09099 7.92133L6.90901 7.92133C6.76984 7.9212 6.63642 7.86582 6.53807 7.76735L0.106047 1.33533C0.0383834 1.27182 9.01957e-07 1.18316 9.06013e-07 1.09036C9.10069e-07 0.997567 0.0383834 0.908904 0.106047 0.8454L0.847934 0.103513C0.913642 0.0372638 1.00309 -5.68113e-07 1.0964 -5.64034e-07C1.18971 -5.59956e-07 1.27915 0.0372638 1.34486 0.103513L7 5.75865L12.6551 0.103514Z"
+									fill="black"
+								/>
+							</svg>
+						</div>
+						<div :class="isVisible ? 'visible' : ''" class="options">
+							<ul>
+								<li
+									v-for="(selectedCategory, index) in selectedCategorys"
+									:key="index"
+									@click="
+										selectItem(selectedCategory.name),
+											sortByCategory(selectedCategory.name)
+									"
+								>
+									{{
+										selectedCategory.name.slice(0, 1).toUpperCase() +
+										selectedCategory.name.slice(1)
+									}}
+								</li>
+							</ul>
+						</div>
 					</div>
-				</div>
-				<div>
-					<label for="vol">Price (between 0 and 1000):</label>
-					<input type="range" v-model="range" min="0" max="1000" step="10" />
+					<div>
+						<label for="vol">Price (between 0 and 1000):</label>
+						<input type="range" v-model="range" min="0" max="1000" step="10" />
+					</div>
 				</div>
 			</div>
 			<div class="col-md-9 d-flex flex-wrap justify-content-center">
@@ -123,7 +125,6 @@ export default {
 				{ id: 2, name: "women's clothing" },
 				{ id: 3, name: 'jewelery' },
 				{ id: 4, name: 'electronics' },
-				
 			],
 			shopBys: [{ name: 'payme' }, { name: 'click' }],
 		};
@@ -260,5 +261,12 @@ export default {
 }
 .rotate {
 	transform: rotate(180deg);
+}
+@media (min-width: 768px) {
+	.stiky_x {
+		position: sticky;
+		top: 80px;
+		z-index: 1;
+	}
 }
 </style>
